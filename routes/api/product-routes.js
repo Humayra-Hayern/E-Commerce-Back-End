61074e352ec98ed2f120317b1520211bbb3de541
Humayra Hayern <hayern786@hotmail.com>
2022-07-12 12:57:34 +0100

    deleting product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -127,3 +127,21 @@ router.put("/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+router.delete("/:id", (req, res) => {
+  // delete one product by its `id` value
+  let deletedProduct = Product.findByPk(req.params.id);
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      res.json(`${deletedProduct} was removed from the database`);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
